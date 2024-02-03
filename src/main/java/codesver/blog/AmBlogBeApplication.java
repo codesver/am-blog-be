1fba07ec2092023443b96fda79ef6eccd54e4e3e
codesver <codesver@gmail.com>
2024-02-03 13:53:05 +0900

    init project

---
+++ src/main/java/codesver/blog/AmBlogBeApplication.java
@@ -0,0 +1,13 @@
+package codesver.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmBlogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmBlogBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codesver/blog/AmBlogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package codesver.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmBlogBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
